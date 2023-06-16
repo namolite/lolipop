7a61baee563c7c3014f6dd9b074b5308bccef37a
心慌慌脸红红 <61996135+namolite@users.noreply.github.com>
2023-06-16 15:58:57 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/*',
-        headers: [
-          {
-            key: 'x-custom-header',
-            value: 'my custom header value',
-          },
-          {
-            key: 'x-another-custom-header',
-            value: 'my other custom header value',
-          },
-        ],
-      },
-    ];
-  },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
