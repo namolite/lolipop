3d46675a0c5841fb8c3f34391adf8e40a9474e1c
loli <namolite@foxmail.com>
2023-09-14 10:39:16 +0800

    Upload resume nya.

--- next.config.js
+++ next.config.js
@@ -2,19 +2,6 @@
 const nextConfig = {
   async headers() {
     return [
-      {
-        source: '/*',
-        headers: [
-          {
-            key: 'x-custom-header',
-            value: 'my custom header value',
-          },
-          {
-            key: 'x-another-custom-header',
-            value: 'my other custom header value',
-          },
-        ],
-      },
     ];
   },
 }
